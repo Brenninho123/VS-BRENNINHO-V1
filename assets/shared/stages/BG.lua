b46c1b38750dc17ba68e1a70986cc3bb45a8b147
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2024-08-24 14:27:57 -0300

    Add files via upload

---
+++ assets/shared/stages/BG.lua
@@ -0,0 +1,5 @@
+function onCreate()
+	makeLuaSprite('BG', 'BG', -400, -750);
+	scaleLuaSprite('BG', 0.9, 0.9); 
+	addLuaSprite('BG', false);
+end
---
+++ assets/shared/stages/Branco.lua
@@ -0,0 +1,5 @@
+function onCreate()
+	makeLuaSprite('Branco', 'Branco', -400, -750);
+	scaleLuaSprite('Branco', 0.9, 0.9); 
+	addLuaSprite('Branco', false);
+end
